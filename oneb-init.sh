8a917cfcf331a91120f6f107231f0a4a96e50730
Justin Schwab <jgschwab@ncsu.edu>
2019-02-06 17:30:49 -0500

    finish oneb init script

--- oneb-init.sh
+++ oneb-init.sh
@@ -4,5 +4,12 @@ sudo chmod +x oneb/*
 sudo apt-get update
 sudo apt-get install -y gnu-smalltalk
 cd oneb/
-echo "--------------Test Results-----------------"
-gst my.st num.st testNum.st eject.st testEject.st b4now.st testB4Now.st myvisit.st testVisit.st
+echo "--------------Test Output-----------------"
+echo "------1b1------"
+gst my.st num.st testNum.st
+echo "------1b2------"
+gst my.st eject.st testEject.st
+echo "------1b3------"
+gst my.st b4now.st testB4Now.st
+echo "------1b4------"
+gst my.st myvisit.st testVisit.st
