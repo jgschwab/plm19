9fe976064a8d228637f2679b98b332c3a1fb27d3
Jeremy Schmidt <jpschmid@ncsu.edu>
2019-03-30 20:10:59 -0400

    Switch to mock server setup

--- project2/duo/src/dom_server.py
+++ project2/duo/src/dom_server.py
@@ -1,15 +1,31 @@
-from flask import (Flask, request, jsonify)
-
-app = Flask(__name__)
+import json
 
 
-@app.route("/domScoredData", methods=["POST"])
-def dom_score_from_request():
-    request_data = request.get_json()
+def dom_score_handler(method, json_data):
+    print(json_data)
+    if method == "POST":
+        rows = DataRows(json.loads(json_data))
+        return json.dumps(calculate_dom_score(rows)), 200
+    else:
+        return "Unsupported method", 405
 
-    rows = DataRows(request_data)
 
-    return jsonify(calculate_dom_score(rows))
+handlers = {
+        "domScoredData": dom_score_handler
+        }
+
+
+def mock_request(method, endpoint, *args):
+    """
+    Main interface to server.
+    Simulates an http call with the given method at the specified url
+    Always returns string response and integer status code
+    """
+    handler_function = handlers.get(endpoint)
+    if not handler_function:
+        return "Request handler not found", 404
+    else:
+        return handler_function(method, args[0])
 
 
 def calculate_dom_score(data):
@@ -24,7 +40,3 @@ class DataRows:
     def __init__(self, json_data):
         self.rows = json_data["rows"]
         self.weights = json_data["weights"]
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
